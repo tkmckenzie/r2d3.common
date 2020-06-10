7d1da79277f50af0fcbffaa65e93e01a990371f3
tmcken <tmcken@sandia.gov>
2020-06-10 15:58:02 -0600

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Energy sources and sinks
+#' 
+#' Dataset describing energy generation and consumption
+#' as a directed network. Data come from the Department
+#' of Energy & Climate Change via Tom Counsell. See
+#' \url{http://www.decc.gov.uk/en/content/cms/tackling/2050/calculator_on/calculator_on.aspx}.
+#' 
+"energy"
